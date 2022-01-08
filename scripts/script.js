a215717ee10a9467f6d4ad6200c57f8b6340e40e
Alex Turbin <turbin28@gmail.com>
2022-01-09 00:45:14 +0300

    feat: put the GameService on file

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ import * as Ma from './MyArray'; // работа с массивом
 import * as MText from './MyText'; // работа с текстом
 import * as MP from './MyPoint';
 
+import * as MyGameService from './MyGameService';
 
 
 (async function () {  // Enables async/await in JS [part 1]
@@ -30,9 +31,7 @@ import * as MP from './MyPoint';
   let text2DCl = new MText.MyText(text2D) // инициализация класса
   let dejstv = "zero"
 
-  function vuvodInformacii(info = 'no info') {
-    text2DCl.test(`${info}`)
-  }
+  function vuvodInformacii(info = 'no info') { text2DCl.test(`${info}`) }
 
 
 
@@ -168,121 +167,11 @@ import * as MP from './MyPoint';
   let topCardNumber4 = new Osc.OnScene(topCard4, mattopCard4, textureArrayScecond)
 
   //
-
-  let GameService = {
-    point: 0,
-    pointMin: 0,
-    pointMax: 20,
-
-    health: 3,
-    healthMin: 0,
-    healthMax: 3,
-
-    step: 0,
-    stepMax: 0,
-    stepMin: 0,
-
-    isGame: true,
-    isReadyToAnswer: false,// ne ispolzuetsa UDALIT
-    howEnd: null,
-
-    gameCycleResult: false,
-
-    mainButtonPosition: 0,
-    mainButtonOptions: ['start', 'stop', 'reset',],
-    mainButtonFunction: [0, 1, 2],
-
-    get monitor() {
-      let texOut =
-        `p: ${this.pointGet} h: ${this.healthGet} s: ${this.step}
-      isisGame: ${this.isGame}  ${this.howEnd}
-      mainBOp: ${this.mainButtonOptions[this.mainButtonPosition]}
-      `
-      return texOut
-    },
-
-    endGame(func = null) {
-      this.howEnd = func
-      this.isGame = false
-      if (func === 'win') { this.mainButtonPosition = 2 }
-      if (func === 'lose') { this.mainButtonPosition = 2 }
-      if (func === 'abort') { this.mainButtonPosition = 1 }
-      Diagnostics.log("END_GAME")
-    },
-
-    tapMainButton() {
-      this.mainButtonPosition++
-      if (this.mainButtonPosition > this.mainButtonOptions.length - 1) {
-        this.mainButtonPosition = 0
-      }
-    },
-
-    //RESET
-    gameReset() {
-      this.pointZero(),
-        this.healthZero(),
-        this.stepZero(),
-        this.isGame = true,
-        this.isReadyToAnswer = false
-      this.howEnd = null
-    },
-
-    //POINT
-    get pointGet() {
-      return this.point
-    },
-
-    pointZero() {
-      this.pointSet = this.pointMin
-    },
-
-    pointUp() {
-      this.pointSet = this.pointGet + 1
-    },
-
-    set pointSet(value) {
-      this.point = value
-      if (this.point === this.pointMax) {
-        this.endGame('win')
-        //
-      }
-      numZero.numberPoint(this.point)
-    },
-    //HEALTH
-    get healthGet() {
-      return this.health
-    },
-
-    healthZero() {
-      this.healthSet = this.healthMax
-    },
-
-    healthDown() {
-      this.healthSet = this.healthGet - 1
-    },
-
-    set healthSet(value) {
-      this.health = value
-      if (this.health === this.healthMin) {
-        this.endGame('lose')
-        //
-      }
-      numZero2.numberPoint(this.health)
-    },
-    //STEP
-    stepZero() {
-      this.step = this.stepMin
-    },
-
-    stepUp() {
-      this.stepSet = this.step + 1
-    },
-
-    set stepSet(value) {
-      this.step = value
-    },
-    //
-  }
+  //////////////////////////////////////////////////////////
+  let GameService = MyGameService.GameService;
+  GameService.pointMax = 20
+  GameService.healthMax = 5
+  // Diagnostics.log(GameService)
 
   /////////////////////////////////////////////////////////
 
@@ -306,6 +195,7 @@ import * as MP from './MyPoint';
   //////////////////////////////////////////////////////////
   let GameAction = {
     checkEndGame() {
+      updatePointsHelth(numZero2, GameService, numZero);
       if (GameService.isGame === false) {
         this.endGameFunction()
       } else {
@@ -330,6 +220,7 @@ import * as MP from './MyPoint';
     globalReset() {
       GameService.gameReset()
       GameCore.preparation()
+      updatePointsHelth(numZero2, GameService, numZero);
       //animReset
     },
 
@@ -546,4 +437,9 @@ resultCore: ${this.resultCore}`
 
 
 })();
+
+function updatePointsHelth(numZero2, GameService, numZero) {
+  numZero2.numberPoint(GameService.health);
+  numZero.numberPoint(GameService.point);
+}
 
